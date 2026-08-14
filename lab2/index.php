8678cc92b3a93b5cba3fb415e305eab423104ab6
zefan310 <ynzonstefan@gmail.com>
2026-08-14 12:51:04 +0800

    Lab2 (#2)
    * Added lab2 folder
    
    * Added index.php, page2.php, page3.php

--- lab2/index.php
+++ lab2/index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
+<?php
+    $pageTitle = "Home - Page 1";
+    $pageHeader = "Home - Page 1";
+?>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $pageTitle; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $pageHeader; ?></h1>
+        <p>This will be the home page. There will be a description on what this site is about and links to other pages on the site.</p>
+
+        <img src="https://preview.redd.it/not-gonna-lie-we-all-built-a-dirt-house-once-or-more-in-our-v0-i10nna6mb0ld1.jpeg?auto=webp&s=195b0c1ce261f87ba1a22bb9ee3a0f9611f81e28" alt="Page 1 Image">
+
+        <nav>
+            <h3>Navigation</h3>
+            <ul>
+                <li><a href="page2.php">Go to Page 2</a></li>
+                <li><a href="page3.php">Go to Page 3</a></li>
+            </ul>
+        </nav>
+    </body>
 </html>
---
+++ lab2/page3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+    $pageTitle = "Education - Page 3";
+    $pageHeader = "Education - Page 3";
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $pageTitle; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $pageHeader; ?></h1>
+        <p>This will be the Education page. This page will be mostly about my education like what schools I attended and what organizations I joined. There will still be links to other pages on the site.</p>
+
+        <img src="https://i.redd.it/hejskezh7ecg1.gif" alt="Page 3 image">
+
+        <nav>
+            <h3>Navigation</h3>
+            <ul>
+                <li><a href="index.php">Go to Page 1</a></li>
+                <li><a href="page2.php">Go to Page 2</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
