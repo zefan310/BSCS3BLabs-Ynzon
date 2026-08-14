574b1dbc4118f49289d2c55d90c7cbc89357ea27
Stefan Ynzon Home <ynzonstefan@gmail.com>
2026-08-14 12:48:25 +0800

    Added index.php, page2.php, page3.php

---
+++ lab2/page2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+    $pageTitle = "Early Life - Page 2";
+    $pageHeader = "Early Life - Page 2";
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $pageTitle; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $pageHeader; ?></h1>
+        <p>This will be the Early Life page. This page will be mostly about the early stages of my life like my childhood. There will still be links to other pages on the site.</p>
+
+        <img src="https://s.yimg.com/lo/mysterio/api/8eeb79bd2134b00d4da7f85864b83ac02759f804864e10d3990dc1cea2811af5/lightyear_networkapi/resizefill_w800_h450%3Bquality_80%3Bformat_webp/https%3A%2F%2Fmedia.zenfs.com%2Fen%2Fknow_your_meme_909%2Fc84361008686f16fb37b1a4dbaac0b83" alt="Page 2 image">
+
+        <nav>
+            <h3>Navigation</h3>
+            <ul>
+                <li><a href="index.php">Go to Page 1</a></li>
+                <li><a href="page3.php">Go to Page 3</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
